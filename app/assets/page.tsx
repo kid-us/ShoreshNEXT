ce37733cdf1c8bc8f3f3c139f9dda3da770d30fc
kid-us <kidushh29@gmail.com>
2025-02-28 16:02:29 +0300

    Used Suspense

--- app/assets/page.tsx
+++ app/assets/page.tsx
@@ -1,12 +1,17 @@
 import { Metadata } from "next";
 import AllAssets from "./AllAssets";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: { absolute: "Our Assets" },
 };
 
 const AssetsPage = () => {
-  return <AllAssets />;
+  return (
+    <Suspense>
+      <AllAssets />
+    </Suspense>
+  );
 };
 
 export default AssetsPage;

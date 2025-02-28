033811fd68729febe83df0367e73a5717071ed95
kid-us <kidushh29@gmail.com>
2025-02-28 12:12:19 +0300

    Key id fixed.

--- app/services/footer.ts
+++ app/services/footer.ts
@@ -3,34 +3,34 @@ import { Nav } from "./nav";
 export const footer: Nav[] = [
   { id: 1, name: "About US", path: "/about-us" },
 
-  { id: 4, name: "Investor Portal", path: "/investor-portal" },
+  { id: 2, name: "Investor Portal", path: "/investor-portal" },
 
-  { id: 2, name: "Contact us", path: "/contact" },
+  { id: 3, name: "Contact us", path: "/contact" },
 
   {
-    id: 3,
+    id: 4,
     name: "Our Assets",
     path: "/assets?asset=our-assets",
   },
   {
-    id: 4,
+    id: 6,
     name: "For Sale",
     path: "/assets?asset=for-sale",
   },
   {
-    id: 5,
+    id: 7,
     name: "Recently Sold",
     path: "/assets?asset=sold",
   },
 
   {
-    id: 6,
+    id: 8,
     name: "Privacy Policy",
     path: "/",
   },
 
   {
-    id: 7,
+    id: 9,
     name: "Terms of Service",
     path: "/",
   },

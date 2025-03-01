c482d441abb13d32938f14675c8612828dd40c7b
kid-us <kidushh29@gmail.com>
2025-03-01 10:36:38 +0300

    Animate style package added for the menu animation.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Playfair_Display, Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 import "./app.css";
+import "animate.css";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
